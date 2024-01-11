0e09b623a0b452e50d8bf7fe2da07bc5140a5f67
Ngọc Linh <linhlinhbehy@gmail.com>
2024-01-11 23:22:11 +0700

    bo sung bai 3

--- BT03/Program.cs
+++ BT03/Program.cs
@@ -24,16 +24,36 @@ namespace BT03
                 {
                     case 1:
                         Console.WriteLine("Chon khoi thi");
+                        Console.WriteLine("(A/B/C)");
+
                         char khoithi =char.Parse( Console.ReadLine());
 
                         ThiSinh thisinh;
                         switch(khoithi)
                         {
                             case 'A':
-                                thisinh=new thi
+                                thisinh=new TSKhoiA();
+                                break
+                            case'B':
+                                thisinh =new TSKhoiB();
+                                break;
+                            case'C':
+                                thisinh=new TSKhoiC();
+                                break;
+
                         }
+                        thisinh.NhapThongTin();
+                        tuyensinh.ThemMoiThiSinh(thisinh);
+                        break;
+                    case 2:
+                        Console.WriteLine("Danh sach thisinh");
+                        tuyensinh.HienThiDS();
+                        break;
+                    case 3:
+                        Console.WriteLine("Thoat chuong trinh");
+                        break;
                 }
-            }
+            }while(chon!=0);
 
         }
     }
